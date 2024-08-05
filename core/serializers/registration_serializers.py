ae0206d0f5296975bc8d3a9e954667a4f03c8387
Ali8Bits <the.ali8bits@gmail.com>
2024-08-05 02:56:33 -0700

    the error messages has been updated

--- core/serializers/registration_serializers.py
+++ core/serializers/registration_serializers.py
@@ -1,4 +1,3 @@
-from django.core.validators import EmailValidator
 from rest_framework import serializers
 from django.contrib.auth import get_user_model, authenticate
 import re
@@ -11,10 +10,6 @@ class SignupSerializer(serializers.ModelSerializer):
     phone_number = serializers.CharField(max_length=20, required=True, label='شماره تلفن')
     password = serializers.CharField(write_only=True, required=True, label='رمز عبور')
 
-    # first_name = serializers.CharField(write_only=True, label='نام')
-    # last_name = serializers.CharField(write_only=True, label="نام خانوادگی")
-    # email = serializers.EmailField(write_only=True, allow_blank=True, label="ایمیل")
-
     class Meta:
         model = User
         fields = [
@@ -26,46 +21,25 @@ class SignupSerializer(serializers.ModelSerializer):
     def validate_phone_number(self, value):
         persian_phone_number_pattern = r'^09[0-9]{9}$'
         if not re.match(persian_phone_number_pattern, value):
-            raise serializers.ValidationError('شماره تلفن وارد شده معتبر نمی باشد!')
+            raise serializers.ValidationError({'message': '!شماره تلفن وارد شده معتبر نمی باشد'})
         elif User.objects.filter(username=value).exists():
-            raise serializers.ValidationError('کاربری با شماره تلفن وارد شده وجود دارد!')
+            raise serializers.ValidationError({'message': '!کاربری با شماره تلفن وارد شده وجود دارد'})
         return value
 
     def validate_password(self, value):
         if len(value) < 8:
-            raise serializers.ValidationError('رمز عبور باید حداقل ۸ کاراکتر باشد!')
+            raise serializers.ValidationError({'message': '!رمز عبور باید حداقل ۸ کاراکتر باشد'})
         return value
 
-    # def validate_email(self, value):
-    #     allowed_domains = ['gmail.com', 'yahoo.com', 'outlook.com']
-    #     if value:
-    #         domain = value.split('@')[1]
-    #         if domain not in allowed_domains:
-    #             raise serializers.ValidationError(
-    #                 f'ایمیل باید یکی از این دامنه‌ها را داشته باشد: {", ".join(allowed_domains)}')
-    #
-    #     return value
-
-    # def validate_last_name(self, value):
-    #     first_name = self.initial_data.get('first_name')
-    #     if value == first_name:
-    #         raise serializers.ValidationError('نام خانوادگی نمی تواند با نام یکی باشد')
-    #     return value
-
-    # write and apply validaton for all data
     def validate(self, data):
-        first_name = data.get('first_name')
-        last_name = data.get('last_name')
         email = data.get('email')
         allowed_domains = ['gmail.com', 'yahoo.com', 'outlook.com']
 
-        # if last_name == first_name:
-        #     raise serializers.ValidationError('نام خانوادگی نمی تواند با نام یکی باشد')
         if email:
             domain = email.split('@')[1]
             if domain not in allowed_domains:
-                raise serializers.ValidationError(
-                    f'ایمیل باید یکی از این دامنه‌ها را داشته باشد: {", ".join(allowed_domains)}')
+                error_message = f'ایمیل باید یکی از این دامنه‌ها را داشته باشد: {", ".join(allowed_domains)}'
+                raise serializers.ValidationError({'message': error_message})
         return data
 
     def create(self, validated_data):
@@ -74,25 +48,6 @@ class SignupSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(username=username,
                                         password=password, **validated_data)
         return user
-    # def create(self, validated_data):
-    #     # Extract the password and remove it from validated_data
-    #     password = validated_data.pop('password')
-    #
-    #     # Check if `phone_number` is used as the username
-    #     if 'phone_number' in validated_data:
-    #         username = validated_data.pop('phone_number')
-    #     else:
-    #         # Handle if `phone_number` is not present
-    #         raise serializers.ValidationError("Phone number is required")
-    #
-    #     # Create the user with the appropriate fields
-    #     user = User.objects.create_user(
-    #         username=username,
-    #         password=password,
-    #         **validated_data
-    #     )
-    #
-    #     return user
 
 
 class LoginSerializer(serializers.Serializer):
@@ -102,20 +57,20 @@ class LoginSerializer(serializers.Serializer):
     def validate_phone_number(self, value):
         persian_phone_number_pattern = r'^09[0-9]{9}$'
         if not re.match(persian_phone_number_pattern, value):
-            raise serializers.ValidationError('شماره تلفن وارد شده معتبر نمی باشد!')
+            raise serializers.ValidationError({'message': '!شماره تلفن وارد شده معتبر نمی باشد'})
         elif not User.objects.filter(username=value).exists():
-            raise serializers.ValidationError('کاربری با شماره تلفن وارد شده وجود ندارد!')
+            raise serializers.ValidationError({'message': '!کاربری با شماره تلفن وارد شده وجود ندارد'})
         return value
 
     def validate_password(self, value):
         if len(value) < 8:
-            raise serializers.ValidationError('رمز عبور باید حداقل ۸ کاراکتر باشد!')
+            raise serializers.ValidationError({'message': '!رمز عبور باید حداقل ۸ کاراکتر باشد'})
         return value
 
     def validate(self, data):
         user = authenticate(username=data['phone_number'], password=data['password'])
         if not user:
-            raise serializers.ValidationError('شماره تلفن یا رمز عبور اشتباه است!')
+            raise serializers.ValidationError({'message': '!شماره تلفن یا رمز عبور اشتباه است'})
         return data
 
 
@@ -126,5 +81,5 @@ class RefreshSerializer(serializers.Serializer):
         try:
             RefreshToken(value)
         except Exception as e:
-            raise serializers.ValidationError('توکن نامعتبر است!')
+            raise serializers.ValidationError({'message': '!توکن نامعتبر است'})
         return value
--- words/serializers/category_serializers.py
+++ words/serializers/category_serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from LDL import settings
 from words.models import Category
 
